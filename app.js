1fa1b89b717ae4bf2bd89a4862f56453388d2b85
Lubov Gumennikova <lyubovgumennikova@yandex.ru>
2022-03-22 08:04:40 +0300

    mesro13jh

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-mongoose.connect('mongodb://localhost:27017/mydb');
-// , {
-//   useNewUrlParser: true,
-//   // useCreateIndex: true,
-//   // useFindAndModify: false,
-// });
-
-app.use('/users', require('./routes/users'));
-
-// app.use((req, res, next) => {
-//   req.user = {
-//     _id: '6235188a2594b225bbf4ba33',
-//   };
-
-//   next();
-// });
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
